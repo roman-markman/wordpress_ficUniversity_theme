1072b8ef9c7ee26fe023f78c4b7fb7f7be12e0da
roman-markman <roman.markman@gmail.com>
2020-06-01 19:48:30 -0400

    Added style to the home page & Added new style and JavaScript files

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 function university_files() {
+	wp_enqueue_script('main_university_javascript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
 	wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 	wp_enqueue_style('university_font', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 	wp_enqueue_style('university_main_styles', get_stylesheet_uri());
